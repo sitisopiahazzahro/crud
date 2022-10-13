19fcbf84f838b33055366773b8643af598524705
sitisopiahazzahro <sitisopiahazzahro1711@.com>
2022-10-13 09:52:38 +0700

    crud

---
+++ application/controllers/kampus.php
@@ -0,0 +1,98 @@
+<?php
+class Kampus extends CI_Controller{
+
+    function __construct(){
+        parent:: __construct();
+        $this->load->model('m_data');
+        $this->load->helper('url');
+    }
+
+    function index(){
+        $data['mahasiswa'] = $this->m_data->tampil_data()->result();
+        $this->load->view('tampil_data',$data);
+    }
+
+    function tambah(){
+        $this->load->view('input_data');
+    }
+
+    function tambah_aksi(){
+        $nim = $this->input->post('nim');
+        $nama = $this->input->post('nama');
+        $alamat = $this->input->post('alamat');
+        $pekerjaan = $this->input->post('pekerjaan');
+        
+        $config['max_size']=2048;
+        $config['allowed_types']="png|jpg|jpeg|gif";
+        $config['remove_spaces']=TRUE;
+        $config['overwrite']=TRUE;
+        $config['upload_path']=FCPATH.'images';
+
+        $this->load->library('upload');
+        $this->upload->initialize($config);
+
+        $this->upload->do_upload('foto');
+        $data_image=$this->upload->data('file_name');
+        $location='images/';
+        $foto=$location.$data_image;
+
+        $data = array(
+            'nim' => $nim,
+            'nama' => $nama,
+            'alamat' => $alamat,
+            'pekerjaan' => $pekerjaan,
+            'foto' => $foto
+            );
+        $this->m_data->input_data($data,'mahasiswa');
+        redirect('kampus/index');
+    }
+
+    function edit($id){
+        $where = array('id' => $id);
+        $data['mahasiswa'] = $this->m_data->edit_data($where,'mahasiswa')->result();
+        $this->load->view('edit_data',$data);
+    }
+        function update(){
+        $id = $this->input->post('id');
+        $nim = $this->input->post('nim');
+        $nama = $this->input->post('nama');
+        $alamat = $this->input->post('alamat');
+        $pekerjaan = $this->input->post('pekerjaan');
+        
+        $config['max_size']=2048;
+        $config['allowed_types']="png|jpg|jpeg|gif";
+        $config['remove_spaces']=TRUE;
+        $config['overwrite']=TRUE;
+        $config['upload_path']=FCPATH.'images';
+
+        $this->load->library('upload');
+        $this->upload->initialize($config);
+
+        $this->upload->do_upload('foto');
+        $data_image=$this->upload->data('file_name');
+        $location='images/';
+        $foto=$location.$data_image;
+
+        $data = array(
+            'nim' => $nim,
+            'nama' => $nama,
+            'alamat' => $alamat,
+            'pekerjaan' => $pekerjaan,
+            'foto' => $foto
+        );
+
+        $where = array(
+            'id' => $id
+        );
+
+        $this->m_data->update_data($where,$data,'mahasiswa');
+        redirect('kampus/index');
+    }
+
+    function hapus($id){
+        $where = array('id' => $id);
+        $this->m_data->hapus_data($where,'mahasiswa');
+        redirect('kampus/index');
+    }
+}
+?>
---
+++ db_kampus.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Okt 2022 pada 03.48
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kampus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nim` varchar(8) CHARACTER SET latin1 NOT NULL,
+  `nama` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `alamat` text CHARACTER SET latin1 NOT NULL,
+  `pekerjaan` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `foto` text CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `alamat`, `pekerjaan`, `foto`) VALUES
+(7, '12210800', 'siti sopiah azzahro', 'Tasikmalaya', 'programmer', 'images/SAVE_20210704_185558.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
